dcc8bd9d9562e0153f48903fef5c2cfbe55fbd9e
dsgoers <daniel.goers@cru.org>
2018-03-03 22:22:00 -0500

    Fix a test

--- __tests__/index.ios.js
+++ __tests__/index.ios.js
@@ -1,12 +1,11 @@
-import 'react-native';
-import React from 'react';
+import ReactNative from 'react-native';
 import Index from '../index.ios.js';
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+jest.mock('react-native', () => ({
+    NativeModules: { reactnativeomnitureapi: { trackState: jest.fn() } }
+}))
 
 it('renders correctly', () => {
-  const tree = renderer.create(
-    <Index />
-  );
+
+    Index.trackState();
 });
